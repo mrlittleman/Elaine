839ad6c39871241105b212daaa4d75ec854dd002
Nyx <61583597+mrlittleman@users.noreply.github.com>
2023-12-10 13:29:29 +0800

    Delete Elaine directory

--- Elaine/js/header.js
+++
@@ -1,6 +0,0 @@
-fetch('header.html')
-.then(response => response.text())
-.then(html => {
-    document.getElementById('headerContainer').innerHTML = html;
-})
-.catch(error => console.error('Error fetching header:', error));
